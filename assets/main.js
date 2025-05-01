47acdf7e7e9b1f2f63b4a191349ab808558916d2
msyamrijal <142006436+msyamrijal@users.noreply.github.com>
2025-05-01 14:10:05 +0700

    Update main.js

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,4 @@
-const API_URL = 'URL_APPS_SCRIPT_ANDA';
+const API_URL = https://script.google.com/macros/s/AKfycby29AeoAZFGqTnBxgsqAPYAplE-npavbDqy4pdjbNo/dev;
 
 async function loadJadwal() {
   try {
